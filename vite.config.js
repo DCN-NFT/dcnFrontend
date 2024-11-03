ca768ccd98d51f8cbc55465190394730f1125e2f
jim-les <jimleston35@gmail.com>
2024-11-03 08:41:27 -0800

    updated the wagmi intergration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@wagmi/core/connectors/walletConnect': '@wagmi/core/dist/connectors/walletConnect',
+      '@wagmi/core/providers/jsonRpc': '@wagmi/core/dist/providers/jsonRpc',
+    },
+  },
 })
